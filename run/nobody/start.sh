5f844204334edfadaff95ddb357bf084db8db120
Chris Hassebrook <chasse20@gmail.com>
2021-01-31 23:50:44 -0500

    Update start.sh

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -7,7 +7,7 @@ export JENKINS_USER=nobody
 export JENKINS_HOME=/config
 export JENKINS_WAR=/usr/share/java/jenkins/jenkins.war
 export JENKINS_WEBROOT=--webroot=/var/cache/jenkins
-export JENKINS_PORT=--httpPort=8090
+export JENKINS_PORT=--httpPort=80
 export JENKINS_AJPPORT=--ajp13Port=-1
 export JENKINS_COMMAND_LINE="$JAVA $JAVA_ARGS $JAVA_OPTS -jar $JENKINS_WAR $JENKINS_WEBROOT $JENKINS_PORT $JENKINS_AJPPORT $JENKINS_OPTS"
 
